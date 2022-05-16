815f7b58ddd45fc6df90c9e8c0e1c9e195230d2c
Jorge Cheverria <jorgecheverria96@gmail.com>
2022-05-15 19:08:17 -0500

    Feat: ajuste

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
+    <script src="https://code.jquery.com/jquery-3.5.0.js"></script>
     <title>Clientes</title>
 </head>
 
@@ -18,7 +18,8 @@
                     <div class="card-header">
 
                         <form action="/load-clients">
-                            <button class="btn btn-primary">Cargar clientes</button>
+                            <button class="btn btn-primary" onclick="cargando()">Cargar clientes</button>
+                            <p id="loading"></p>
                         </form>
                     </div>
 
@@ -68,6 +69,12 @@
             </div>
         </div>
     </div>
+
 </body>
+<script>
+    function cargando() {
+        document.getElementById('loading').innerHTML = 'Cargando...';
+    }
+</script>
 
 </html>
